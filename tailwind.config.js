ed44290c0f128e1c291acefb7854b803935a295e
LiviuCauia <li.viu@mail.com>
2022-10-28 08:54:39 +0100

    Style home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,10 @@ module.exports = {
         primary: '#00566b',
         secondary: '#296578',
         border: 'rgba(255,255,255,.3)',
+        purple: '#45235e',
+      },
+      backgroundImage: {
+        'masterhead': "url(https://hrp.imgix.net/https%3A%2F%2Fhistoricroyalpalaces.picturepark.com%2FGo%2FSOu4DO7V%2FV%2F45376%2F1?auto=format&s=48ac294ec04eecca46e95c9e0d5fc8a0)",
       },
     },
   },
